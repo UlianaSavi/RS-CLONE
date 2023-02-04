d345f7bc3242a66ca02495c2faabec2687dc88ae
ilya <shakurov.ia@gmail.com>
2023-02-04 17:42:03 +0300

    feat: create message input component

--- src/components/MessageInput/MessageInput.tsx
+++ src/components/MessageInput/MessageInput.tsx
@@ -1,9 +1,21 @@
 import './MessageInput.scss';
+import { EmojiIcon, AttachIcon, SendMessageIcon } from '../../assets/icons/icons';
 
 function MessageInput() {
   return (
-    <div className="chat-input">
-      MessageInput
+    <div className="message-input">
+      <div className="message-input__container">
+        <button className="message-input__emotion-btn" type="button">
+          <EmojiIcon />
+        </button>
+        <input type="text" className="message-input__text-area" placeholder="Message" />
+        <button className="message-input__attach-btn" type="button">
+          <AttachIcon />
+        </button>
+      </div>
+      <button className="message-input__send-voice-btn" type="button">
+        <SendMessageIcon />
+      </button>
     </div>
   );
 }
