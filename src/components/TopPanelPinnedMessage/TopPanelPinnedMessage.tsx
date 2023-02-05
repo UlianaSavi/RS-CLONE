097e4e8c9f1ae4206a0b10ce9ab8d49d54d57926
Kavalenka Alya <kovalenkoalinam@gmail.com>
2023-02-05 15:24:17 +0300

    feat: add pinned message

--- src/components/TopPanelPinnedMessage/TopPanelPinnedMessage.tsx
+++ src/components/TopPanelPinnedMessage/TopPanelPinnedMessage.tsx
@@ -1,13 +1,20 @@
 import './TopPanelPinnedMessage.scss';
 import { ReactComponent as PinnedListIcon } from '../../assets/icons/pinned-list.svg';
+import { ReactComponent as VerticalLine } from '../../assets/icons/vertical-line.svg';
 
 export default function TopPanelPinnedMessage() {
   return (
     <div className="top-panel-pinned__wrapper">
-      {/* <div className="top-panel-pinned__pinned-messages">
-        <div className="top-panel-pinned__left-border"></div>
-        <div className="top-panel-pinned__message"></div>
-      </div> */}
+      <div className="top-panel-pinned__pinned-messages">
+        <div className="top-panel-pinned__left-border">
+          <VerticalLine className="top-panel-pinned__line active" />
+          <VerticalLine className="top-panel-pinned__line" />
+        </div>
+        <div className="top-panel-pinned__message-content">
+          <div className="top-panel-pinned__pinned-message-text">Pinned Message</div>
+          <div className="top-panel-pinned__message">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, beatae doloremque facere tenetur ratione non, itaque saepe facilis earum alias quisquam culpa quia mollitia doloribus ex repellendus, molestiae dolore impedit.</div>
+        </div>
+      </div>
       <button className="top-panel-pinned__btn" type="button">
         <PinnedListIcon className="top-panel-pinned__pin-icon" />
       </button>
