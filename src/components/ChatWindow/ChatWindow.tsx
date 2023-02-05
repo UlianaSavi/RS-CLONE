5b2146bcec8fe45704f7ed26c03dce041322d9d2
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-02-06 00:22:18 +0300

    feat: add styles and tails for messages from two person

--- src/components/ChatWindow/ChatWindow.tsx
+++ src/components/ChatWindow/ChatWindow.tsx
@@ -1,5 +1,5 @@
 import './ChatWindow.scss';
-import { ReactComponent as MessageTriangle } from '../../assets/icons/messtr.svg';
+// import { ReactComponent as MessageTriangle } from '../../assets/icons/messtr.svg';
 
 function ChatWindow() {
   return (
@@ -9,14 +9,29 @@ function ChatWindow() {
           <div className="bubbles-date-group__date">Today</div>
           <div className="bubbles-date-group__messages-wrapper">
             <div className="bubbles-date-group__user-message-wrapper">
-              <div className="bubbles-date-group__user-message-container">
-                <div className="bubbles-date-group__text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas totam aspernatur quis earum, suscipit mollitia incidunt facere molestiae ducimus quos iste illo harum corrupti eveniet magnam delectus dicta? Voluptatibus, a.</div>
-              </div>
-              <MessageTriangle className="bubbles-date-group__message-triangle" />
+              <div className="bubbles-date-group__user-message">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas totam aspernatur quis earum, suscipit mollitia incidunt facere molestiae ducimus quos iste illo harum corrupti eveniet magnam delectus dicta? Voluptatibus, a.</div>
+              <div className="bubbles-date-group__user-message">Lorem ipsum</div>
+            </div>
+            <div className="bubbles-date-group__user-message-wrapper">
+              <div className="bubbles-date-group__user-message another-user">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas totam aspernatur quis earum, suscipit mollitia incidunt facere molestiae ducimus quos iste illo harum corrupti eveniet magnam delectus dicta? Voluptatibus, a.</div>
+              <div className="bubbles-date-group__user-message another-user">Lorem</div>
+            </div>
+            <div className="bubbles-date-group__user-message-wrapper">
+              <div className="bubbles-date-group__user-message">Lorem ipsum</div>
+            </div>
+            <div className="bubbles-date-group__user-message-wrapper">
+              <div className="bubbles-date-group__user-message another-user">Lorem</div>
             </div>
           </div>
         </section>
       </div>
+      <svg height="0" width="0">
+        <defs>
+          <clipPath id="svgPath">
+            <path fill="#FFFFFF" d="M10,10H0V0H0A10,10,0,0,0,10,10Z" />
+          </clipPath>
+        </defs>
+      </svg>
     </div>
   );
 }
