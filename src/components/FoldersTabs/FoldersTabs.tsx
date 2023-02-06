398bc0c8bc6966c427d39b906da5432053704d50
ilya <shakurov.ia@gmail.com>
2023-02-06 18:48:41 +0300

    feat: add fake data and interactive for folders tabs

--- src/components/FoldersTabs/FoldersTabs.tsx
+++ src/components/FoldersTabs/FoldersTabs.tsx
@@ -1,16 +1,40 @@
+import { useState } from 'react';
 import ChatsFolderItem from '../ChatsFolderItem/ChatsFolderItem';
 import './FoldersTabs.scss';
 
 function FoldersTabs() {
+  const foldersData = [
+    {
+      title: 'All',
+      unreadConversations: 3,
+    },
+    {
+      title: 'Contacts',
+      unreadConversations: 2,
+    },
+    {
+      title: 'Chats',
+      unreadConversations: 1,
+    },
+  ];
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const foldersArr = foldersData
+    .map((folder, index) => (
+      <ChatsFolderItem
+        key={folder.title}
+        data={folder}
+        isActive={index === activeIndex}
+        setActiveIndex={setActiveIndex}
+        index={index}
+      />
+    ));
+
   return (
     <div className="folders-tabs">
       <div className="folders-tabs__wrapper">
-        <ChatsFolderItem />
-        <ChatsFolderItem />
-        <ChatsFolderItem />
-        <ChatsFolderItem />
-        <ChatsFolderItem />
-        <ChatsFolderItem />
+        {foldersArr}
       </div>
     </div>
   );
