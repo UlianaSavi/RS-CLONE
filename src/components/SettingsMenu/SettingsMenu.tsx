bbb744b1e6fc8b96c5cccd3bf5356387a84e1c16
ilya <shakurov.ia@gmail.com>
2023-02-06 11:23:13 +0300

    feat: replace popup items for settings and top panel popups

--- src/components/SettingsMenu/SettingsMenu.tsx
+++ src/components/SettingsMenu/SettingsMenu.tsx
@@ -1,13 +1,17 @@
-import SettingsMenuItem from '../SettingsMenuItem/SettingsMenuItem';
+import PopupMenuItem from '../PopupMenuItem/PopupMenuItem';
+import { SettingsIcon } from '../../assets/icons/icons';
 import './SettingsMenu.scss';
 
 function SettingsMenu(props: { isOpen: boolean, onClose: () => void }) {
   const { isOpen, onClose } = props;
+
+  const click = () => null;
+
   return (
     <nav className={isOpen ? 'popup active' : 'popup'} id="popup" onMouseLeave={() => onClose()}>
-      <SettingsMenuItem />
-      <SettingsMenuItem />
-      <SettingsMenuItem />
+      <PopupMenuItem label="Saved Messages" icon={<SettingsIcon />} handleClick={click} />
+      <PopupMenuItem label="Settings" icon={<SettingsIcon />} handleClick={click} />
+      <PopupMenuItem label="Log Out" icon={<SettingsIcon />} handleClick={click} />
     </nav>
   );
 }
