c9f8ddb851192874c268805c654042399ffa078a
Kavalenka Alya <kovalenkoalinam@gmail.com>
2023-02-06 19:12:34 +0300

    refactor: relocate bubbles-date-group to new component

---
+++ src/components/BubblesDateGroup/BubblesDateGroup.tsx
@@ -0,0 +1,67 @@
+import { ReactComponent as CheckMark } from '../../assets/icons/check-solid.svg';
+import './BubblesDateGroup.scss';
+
+export default function BubblesDateGroup() {
+  return (
+    <section className="bubbles-date-group">
+      <div className="bubbles-date-group__date">Today</div>
+      <div className="bubbles-date-group__messages-wrapper">
+        <div className="bubbles-date-group__user-messages-wrapper">
+          <div className="bubbles-date-group__user-message">
+            <span className="bubbles-date-group__message">Lorem ipsum dolor sit amet consectetur, adipisicing elit. At animi dignissimos inventore corporis, cupiditate cum delectus odit. Optio inventore minima hic facilis nobis molestias recusandae, laboriosam consequatur quo, cum ipsum?  </span>
+            <div className="bubbles-date-group__time-n-check-wrapper">
+              <span className="bubbles-date-group__time">
+                00:23
+              </span>
+              <CheckMark className="bubbles-date-group__check-mark" />
+              <CheckMark className="bubbles-date-group__check-mark second-check" />
+            </div>
+          </div>
+          <div className="bubbles-date-group__user-message">
+            <span className="bubbles-date-group__message">
+              Lorem
+            </span>
+            <div className="bubbles-date-group__time-n-check-wrapper">
+              <span className="bubbles-date-group__time">
+                00:23
+              </span>
+              <CheckMark className="bubbles-date-group__check-mark" />
+              <CheckMark className="bubbles-date-group__check-mark second-check" />
+            </div>
+          </div>
+        </div>
+        <div className="bubbles-date-group__user-messages-wrapper">
+          <div className="bubbles-date-group__user-message another-user">
+            <span className="bubbles-date-group__message">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, distin
+            </span>
+            <span className="bubbles-date-group__time">
+              00:23
+            </span>
+          </div>
+          <div className="bubbles-date-group__user-message another-user">
+            <span className="bubbles-date-group__message">
+              Lorem
+            </span>
+            <span className="bubbles-date-group__time">
+              00:23
+            </span>
+          </div>
+        </div>
+        <div className="bubbles-date-group__user-messages-wrapper">
+          <div className="bubbles-date-group__user-message">
+            <span className="bubbles-date-group__message">
+              Lorem ipsum
+            </span>
+            <div className="bubbles-date-group__time-n-check-wrapper">
+              <span className="bubbles-date-group__time">
+                00:23
+              </span>
+              <CheckMark className="bubbles-date-group__check-mark" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/components/ChatWindow/ChatWindow.tsx
+++ src/components/ChatWindow/ChatWindow.tsx
@@ -1,71 +1,12 @@
 import './ChatWindow.scss';
-import { ReactComponent as CheckMark } from '../../assets/icons/check-solid.svg';
 import MessageInput from '../MessageInput/MessageInput';
+import BubblesDateGroup from '../BubblesDateGroup/BubblesDateGroup';
 
 function ChatWindow() {
   return (
     <div className="chat-window">
       <div className="chat-window__wrapper">
-        <section className="bubbles-date-group">
-          <div className="bubbles-date-group__date">Today</div>
-          <div className="bubbles-date-group__messages-wrapper">
-            <div className="bubbles-date-group__user-messages-wrapper">
-              <div className="bubbles-date-group__user-message">
-                <span className="bubbles-date-group__message">Lorem ipsum dolor sit amet consectetur, adipisicing elit. At animi dignissimos inventore corporis, cupiditate cum delectus odit. Optio inventore minima hic facilis nobis molestias recusandae, laboriosam consequatur quo, cum ipsum?  </span>
-                <div className="bubbles-date-group__time-n-check-wrapper">
-                  <span className="bubbles-date-group__time">
-                    00:23
-                  </span>
-                  <CheckMark className="bubbles-date-group__check-mark" />
-                  <CheckMark className="bubbles-date-group__check-mark second-check" />
-                </div>
-              </div>
-              <div className="bubbles-date-group__user-message">
-                <span className="bubbles-date-group__message">
-                  Lorem
-                </span>
-                <div className="bubbles-date-group__time-n-check-wrapper">
-                  <span className="bubbles-date-group__time">
-                    00:23
-                  </span>
-                  <CheckMark className="bubbles-date-group__check-mark" />
-                  <CheckMark className="bubbles-date-group__check-mark second-check" />
-                </div>
-              </div>
-            </div>
-            <div className="bubbles-date-group__user-messages-wrapper">
-              <div className="bubbles-date-group__user-message another-user">
-                <span className="bubbles-date-group__message">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, distin
-                </span>
-                <span className="bubbles-date-group__time">
-                  00:23
-                </span>
-              </div>
-              <div className="bubbles-date-group__user-message another-user">
-                <span className="bubbles-date-group__message">
-                  Lorem
-                </span>
-                <span className="bubbles-date-group__time">
-                  00:23
-                </span>
-              </div>
-            </div>
-            <div className="bubbles-date-group__user-messages-wrapper">
-              <div className="bubbles-date-group__user-message">
-                <span className="bubbles-date-group__message">
-                  Lorem ipsum
-                </span>
-                <div className="bubbles-date-group__time-n-check-wrapper">
-                  <span className="bubbles-date-group__time">
-                    00:23
-                  </span>
-                  <CheckMark className="bubbles-date-group__check-mark" />
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
+        <BubblesDateGroup />
       </div>
       <MessageInput />
       <svg height="0" width="0">
