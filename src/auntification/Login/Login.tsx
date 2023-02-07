06a2dea5ea7aede11ce9c0ba59604e3c46da34bf
UlianaSavi <uljasha.sav@gmail.com>
2023-02-07 12:58:41 +0600

    refactor: delete trash

--- src/auntification/Login/Login.tsx
+++ src/auntification/Login/Login.tsx
@@ -1,20 +1,3 @@
-import { useState } from 'react';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom';
-
-export const signInWithGoogle = async () => {
-  const auth = getAuth();
-  const navigate = useNavigate();
-  const [authing, setAuthing] = useState(false);
-  setAuthing(true);
-
-  signInWithPopup(auth, new GoogleAuthProvider())
-    .then((response) => {
-      console.log(response.user.uid);
-      navigate('/');
-    })
-    .catch((error) => {
-      console.log(error, authing);
-      setAuthing(false);
-    });
+export const signUpWithEmail = async () => {
+// code here
 };
