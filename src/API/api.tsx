05ea5ce1af3e7bc9a14c4b6c1a67b58d4e6944cf
ilya <shakurov.ia@gmail.com>
2023-02-09 20:22:06 +0300

    feat: add functionality when user did not add avatar

--- src/API/api.tsx
+++ src/API/api.tsx
@@ -1,5 +1,5 @@
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from 'firebase/auth';
-import { doc, setDoc } from 'firebase/firestore';
+import { doc, setDoc, updateDoc } from 'firebase/firestore';
 import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 import { auth, db, storage } from '../firebaseConfig';
 
@@ -14,33 +14,37 @@ export const singUp = async (
     const storageRef = ref(storage, `${name}${Math.floor(100000 + Math.random() * 900000)}`);
     const uploadTask = uploadBytesResumable(storageRef, avatar as File);
 
-    uploadTask.on(
-      'state_changed',
-      (snapshot) => {
-        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log(progress);
-      },
-      (error) => {
-        throw error;
-      },
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
-          console.log(downloadURL);
-          await updateProfile(user, {
-            displayName: name,
-            photoURL: downloadURL,
-          });
+    await setDoc(doc(db, 'users', user.uid), {
+      uid: user.uid,
+      name,
+      email,
+      avatarURL: '',
+      isOnline: true,
+    });
 
-          await setDoc(doc(db, 'users', user.uid), {
-            uid: user.uid,
-            name,
-            email,
-            avatarURL: downloadURL,
-            isOnline: true,
+    if (avatar) {
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(progress);
+        },
+        (error) => {
+          throw error;
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            console.log(downloadURL);
+            await updateProfile(user, {
+              photoURL: downloadURL,
+            });
+            await updateDoc(doc(db, 'users', user.uid), {
+              avatarURL: downloadURL,
+            });
           });
-        });
-      },
-    );
+        },
+      );
+    }
   } catch (error) {
     console.log(error);
   }
