05a7f788452c8f99a3f6b72ed7ab6b9ab98ccb43
UlianaSavi <uljasha.sav@gmail.com>
2023-02-09 16:59:39 +0600

    feat&refactor: change message input to teatarea, add change audio/text message logic

--- src/components/MessageInput/MessageInput.tsx
+++ src/components/MessageInput/MessageInput.tsx
@@ -1,15 +1,28 @@
 import { useState } from 'react';
 import AttachPopup from '../AttachPopup/AttachPopup';
 import EmotionPopup from '../EmotionPopup/EmotionPopup';
-import { EmojiIcon, AttachIcon, SendMessageIcon } from '../../assets/icons/icons';
+import {
+  EmojiIcon, AttachIcon, SendMessageIcon, AudioMessageIcon,
+} from '../../assets/icons/icons';
 import './MessageInput.scss';
 
 function MessageInput() {
   const [isVisibleAttach, setVisibilityAttach] = useState(false);
   const [isVisibleEmotion, setVisibilityEmotion] = useState(false);
+  const [messageValue, setMessageValue] = useState('');
+  const [isAudio, setIsAudio] = useState(false);
 
   const toggleAttachPopup = () => setVisibilityAttach(!isVisibleAttach);
   const toggleEmotionPopup = () => setVisibilityEmotion(!isVisibleEmotion);
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMessageValue(e.target.value);
+    if (messageValue === '' || e.target.value === '') {
+      setIsAudio(!isAudio);
+    } else {
+      setIsAudio(isAudio);
+    }
+  };
 
   return (
     <div className="message-input">
@@ -18,14 +31,16 @@ function MessageInput() {
           <EmojiIcon />
         </button>
         <EmotionPopup isVisible={isVisibleEmotion} handleMouseLeave={toggleEmotionPopup} />
-        <input type="text" className="message-input__text-area" placeholder="Message" />
+        <textarea className="message-input__text-area" placeholder="Message" value={messageValue} onChange={handleChange} />
         <button className="message-input__attach-btn" type="button" onClick={toggleAttachPopup}>
           <AttachIcon />
         </button>
         <AttachPopup isVisible={isVisibleAttach} handleMouseLeave={toggleAttachPopup} />
       </div>
       <button className="message-input__send-btn" type="button">
-        <SendMessageIcon />
+        {
+          isAudio ? <SendMessageIcon /> : <AudioMessageIcon />
+        }
       </button>
     </div>
   );
