9a3442eb2fe47a28f06ea902810e8700dc723770
UlianaSavi <uljasha.sav@gmail.com>
2023-02-13 19:48:36 +0600

    feat: add checking onPopState

--- src/auth/Auth.tsx
+++ src/auth/Auth.tsx
@@ -8,23 +8,28 @@ import { auth } from '../firebaseConfig';
 function AuthRoute(props: any) {
   const navigate = useNavigate();
   const { children } = props;
-
-  useEffect(() => {
+  function check() {
     const AuthCheck = onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        navigate('/register');
+      }
       if (user) {
         const { uid } = user;
         if (uid) {
           navigate('/');
         }
-      } else {
-        console.log('Unauthorized');
-        navigate('/register');
       }
     });
-
     return () => AuthCheck();
-  }, [auth]);
+  }
 
+  onpopstate = () => {
+    check();
+  };
+
+  useEffect(() => {
+    check();
+  }, [auth]);
   return children;
 }
 export default AuthRoute;
