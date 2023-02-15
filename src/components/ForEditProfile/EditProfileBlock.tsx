4fd85ec19e465f2140a10d25c7d44db3d6ea05e8
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-02-15 16:29:48 +0300

    fix: change styles like in k version

--- src/components/ForEditProfile/EditProfileBlock.tsx
+++ src/components/ForEditProfile/EditProfileBlock.tsx
@@ -14,7 +14,7 @@ export default function EditProfileBlock({ handleEditClick }: SettingsHeaderProp
         <button type="button" className="header__arrow" onClick={handleEditClick}>
           <ArrowLeftIcon />
         </button>
-        <h3 className="header__text">Edit profile</h3>
+        <h3 className="header__text">Edit Profile</h3>
       </div>
       <section className="edit-user-info">
         <div className="edit-user-info__img">
@@ -24,24 +24,23 @@ export default function EditProfileBlock({ handleEditClick }: SettingsHeaderProp
           </button>
         </div>
         <form className="edit-user-info__input-wrapper" id="first-name">
-          <input type="text" value={currentUser.displayName} className="edit-user-info__input" placeholder="First Name" />
-          <label htmlFor="first-name" className="edit-user-info__label">First name (required)</label>
+          <input type="text" value={currentUser.displayName} className="edit-user-info__input" placeholder="Name" />
+          <label htmlFor="first-name" className="edit-user-info__label">Name</label>
         </form>
         <form className="edit-user-info__input-wrapper" id="last-name">
           <input type="text" placeholder="Last name" className="edit-user-info__input" />
-          <label htmlFor="last-name" className="edit-user-info__label">Last name (optional)</label>
+          <label htmlFor="last-name" className="edit-user-info__label">Last Name</label>
         </form>
         <form className="edit-user-info__input-wrapper" id="bio">
-          <input type="text" placeholder="Bio" className="edit-user-info__input" />
-          <label htmlFor="bio" className="edit-user-info__label">Bio</label>
-          <div className="edit-user-info__max-length">70</div>
+          <input type="text" placeholder="Bio (optional)" className="edit-user-info__input" />
+          <label htmlFor="bio" className="edit-user-info__label">Bio (optional)</label>
         </form>
-        <div className="edit-user-info__descr">
-          Any details such as age, occupation or city.
-          <br />
-          Example: 23 y.o. designer from San Francisco
-        </div>
       </section>
+      <div className="edit-user-info__descr">
+        Any details such as age, occupation or city.
+        <br />
+        Example: 23 y.o. designer from San Francisco
+      </div>
     </div>
   );
 }
