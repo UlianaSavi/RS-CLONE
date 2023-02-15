53038d19731a62e42103d0a81b8dcdc8ebfff60d
UlianaSavi <uljasha.sav@gmail.com>
2023-02-15 21:45:01 +0600

    feat: add adding choosed photo into div

--- src/components/AttachPopup/AttachPopup.tsx
+++ src/components/AttachPopup/AttachPopup.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PopupMenuItem from '../PopupMenuItem/PopupMenuItem';
 import './AttachPopup.scss';
 import { ReactComponent as ImageNVideoIcon } from '../../assets/icons/image-n-video.svg';
@@ -11,20 +11,38 @@ interface AttachPopupProps {
 
 function AttachPopup({ isVisible, handleMouseLeave }: AttachPopupProps) {
   const hiddenPhotoInput = React.useRef<HTMLInputElement>(null);
+  const hiddenDocInput = React.useRef<HTMLInputElement>(null);
+  const [photoUrl, setPhotoUrl] = useState<string | ArrayBuffer | null>(null);
 
-  const click = () => {
+  const choosePhoto = () => {
     if (hiddenPhotoInput.current) {
       hiddenPhotoInput.current.click();
     }
   };
 
-  const handleChange = () => null;
+  const chooseFile = () => {
+    if (hiddenDocInput.current) {
+      hiddenDocInput.current.click();
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const fileReader = new FileReader();
+      fileReader.onload = () => {
+        setPhotoUrl(fileReader.result);
+      };
+      fileReader.readAsDataURL(e.target.files[0]);
+    }
+  };
 
   return (
     <nav className={`attach-popup ${isVisible ? 'active' : ''}`} onMouseLeave={handleMouseLeave}>
-      <PopupMenuItem label="Photo" icon={<ImageNVideoIcon />} handleClick={click} />
-      <input className="input-file" type="file" id="upload" name="upload" ref={hiddenPhotoInput} onChange={handleChange} />
-      <PopupMenuItem label="Document" icon={<DocumentIcon />} />
+      <PopupMenuItem label="Photo" icon={<ImageNVideoIcon />} handleClick={choosePhoto} />
+      <input className="input-file" type="file" id="uploadPhoto" ref={hiddenPhotoInput} onChange={handleChange} />
+      <PopupMenuItem label="Document" icon={<DocumentIcon />} handleClick={chooseFile} />
+      <input className="input-file" type="file" id="uploadFile" ref={hiddenDocInput} onChange={handleChange} />
+      <img src={photoUrl?.toString()} alt="" />
     </nav>
   );
 }
