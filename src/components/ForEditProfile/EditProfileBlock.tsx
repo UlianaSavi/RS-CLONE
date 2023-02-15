76249c5f805868bc040c2a89d5bd9966b7e598ce
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-02-15 17:39:52 +0300

    refactor: change forms on formInput component

--- src/components/ForEditProfile/EditProfileBlock.tsx
+++ src/components/ForEditProfile/EditProfileBlock.tsx
@@ -1,13 +1,16 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { ArrowLeftIcon, AddPhotoIcon } from '../../assets/icons/icons';
 import './EditProfileBlock.scss';
 import { SettingsHeaderProps } from '../../types';
 import { AuthContext } from '../../context/AuthContext';
 import avatarPlaceholder from '../../assets/icons/avatar-placeholder.png';
 import type { User } from '../../types';
+import FormInput from '../FormInput/FormInput';
 
 export default function EditProfileBlock({ handleEditClick }: SettingsHeaderProps) {
   const currentUser: User = useContext(AuthContext) as User;
+  const [name, setName] = useState(currentUser.displayName);
+  const [bio, setBio] = useState('');
   return (
     <div className="edit-profile">
       <div className="header">
@@ -23,18 +26,9 @@ export default function EditProfileBlock({ handleEditClick }: SettingsHeaderProp
             <AddPhotoIcon />
           </button>
         </div>
-        <form className="edit-user-info__input-wrapper" id="first-name">
-          <input type="text" value={currentUser.displayName} className="edit-user-info__input" placeholder="Name" />
-          <label htmlFor="first-name" className="edit-user-info__label">Name</label>
-        </form>
-        <form className="edit-user-info__input-wrapper" id="last-name">
-          <input type="text" placeholder="Last name" className="edit-user-info__input" />
-          <label htmlFor="last-name" className="edit-user-info__label">Last Name</label>
-        </form>
-        <form className="edit-user-info__input-wrapper" id="bio">
-          <input type="text" placeholder="Bio (optional)" className="edit-user-info__input" />
-          <label htmlFor="bio" className="edit-user-info__label">Bio (optional)</label>
-        </form>
+        <FormInput type="text" id="name" label="Name" value={name.split(',')[0]} setValue={setName} />
+        <FormInput type="text" id="last-name" label="Last Name" value={name.split(',').length > 1 ? name.split(',')[1] : ''} setValue={setName} />
+        <FormInput type="text" id="bio" label="Bio (optional)" value={bio} setValue={setBio} />
       </section>
       <div className="edit-user-info__descr">
         Any details such as age, occupation or city.
