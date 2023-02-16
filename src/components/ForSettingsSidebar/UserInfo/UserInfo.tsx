d32d416f92a05c4ab63d60c8ed6f979693b7da2e
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-02-16 14:19:40 +0300

    feat: add changing photo in settings block

--- src/components/ForSettingsSidebar/UserInfo/UserInfo.tsx
+++ src/components/ForSettingsSidebar/UserInfo/UserInfo.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../../context/AuthContext';
 import type { User } from '../../../types';
 import avatarPlaceholder from '../../../assets/icons/avatar-placeholder.png';
@@ -6,17 +6,36 @@ import avatarPlaceholder from '../../../assets/icons/avatar-placeholder.png';
 import { AddPhotoIcon, InfoIcon, NameUserIcon } from '../../../assets/icons/icons';
 import PopupMenuItem from '../../PopupMenuItem/PopupMenuItem';
 import './UserInfo.scss';
+import { changeProfilePhoto } from '../../../API/api';
 
 function UserInfo() {
   const currentUser: User = useContext(AuthContext) as User;
+  const [user, setUser] = useState(currentUser);
+
+  useEffect(() => {
+    setUser(user);
+  }, [currentUser.photoURL]);
+
   return (
     <div className="user-info">
       <div className="user-info__wrapper">
-        <img className="user-info__wrapper__ava" src={currentUser.photoURL !== null ? currentUser.photoURL : avatarPlaceholder} alt="User" />
+        <img className="user-info__wrapper__ava" src={user.photoURL || avatarPlaceholder} alt="User" />
         <div className="user-info__wrapper__name">{currentUser.displayName}</div>
         <div className="user-info__wrapper__status">{currentUser.isOnline || 'online'}</div>
         <button type="button" className="icon-button">
           <AddPhotoIcon />
+          <input
+            type="file"
+            className="icon-button__input-file"
+            accept=".jpg, .jpeg, .png"
+            onChange={(event) => {
+              changeProfilePhoto(user.displayName, event.target.files).then((url) => {
+                if (url) {
+                  setUser({ ...user, photoURL: url });
+                }
+              });
+            }}
+          />
         </button>
       </div>
       <div className="user-info__item">
