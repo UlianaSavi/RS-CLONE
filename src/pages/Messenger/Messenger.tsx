dbcfc49388c9bcc65a1bb243a9ec548b2d6a7aee
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-02-18 01:38:07 +0300

    fix: sidebar visible when resize

--- src/pages/Messenger/Messenger.tsx
+++ src/pages/Messenger/Messenger.tsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import Chat from '../../components/Chat/Chat';
 import Sidebar from '../../components/Sidebar/Sidebar';
+import { ActiveChatContext } from '../../context/ActiveChatContext';
 import { SendImageContext } from '../../context/SendImageContext';
 import { ActiveVisibilitySidebar } from '../../context/VisibleSidebarContext';
 import './Messenger.scss';
@@ -8,9 +9,12 @@ import './Messenger.scss';
 function Messenger() {
   const { popap } = useContext(SendImageContext);
   const { isActiveSidebar, setActiveSidebar } = useContext(ActiveVisibilitySidebar);
+  const { activeChatID } = useContext(ActiveChatContext);
 
   window.addEventListener('resize', () => {
     if (window.innerWidth > 920) {
+      setActiveSidebar(true);
+    } else if (activeChatID === '') {
       setActiveSidebar(true);
     } else {
       setActiveSidebar(false);
