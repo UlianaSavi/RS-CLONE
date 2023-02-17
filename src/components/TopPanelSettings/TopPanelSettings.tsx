e8dbeb858c1982dd22b836781444ce863f3de2c0
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-02-18 01:04:46 +0300

    feat: add media for top panel

--- src/components/TopPanelSettings/TopPanelSettings.tsx
+++ src/components/TopPanelSettings/TopPanelSettings.tsx
@@ -3,6 +3,7 @@ import './TopPanelSettings.scss';
 import { ReactComponent as MuteIcon } from '../../assets/icons/mute-bel.svg';
 import { ReactComponent as PhoneIcon } from '../../assets/icons/phone.svg';
 import { ReactComponent as TrashIcon } from '../../assets/icons/trash.svg';
+import { ReactComponent as SearchIcon } from '../../assets/icons/search-icon.svg';
 
 export default function TopPanelSettings(props: { isOpen: boolean, onClose: () => void }) {
   const { isOpen, onClose } = props;
@@ -11,6 +12,9 @@ export default function TopPanelSettings(props: { isOpen: boolean, onClose: () =
 
   return (
     <nav className={isOpen ? 'top-panel-popup active' : 'top-panel-popup'} id="top-panel-popup" onMouseLeave={() => onClose()}>
+      <div className="search-wrapper">
+        <PopupMenuItem label="Search" icon={<SearchIcon />} handleClick={click} />
+      </div>
       <PopupMenuItem label="Mute" icon={<MuteIcon />} handleClick={click} />
       <PopupMenuItem label="Call" icon={<PhoneIcon />} handleClick={click} />
       <PopupMenuItem label="Delete chat" icon={<TrashIcon />} handleClick={click} />
