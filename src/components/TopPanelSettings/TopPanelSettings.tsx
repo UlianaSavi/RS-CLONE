d55ed11c06391c5d7a75703668ed55c7756bf619
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-02-18 15:30:55 +0300

    refactor: relocate icons in icons.tsx

--- src/components/TopPanelSettings/TopPanelSettings.tsx
+++ src/components/TopPanelSettings/TopPanelSettings.tsx
@@ -1,9 +1,9 @@
 import PopupMenuItem from '../PopupMenuItem/PopupMenuItem';
 import './TopPanelSettings.scss';
-import { ReactComponent as MuteIcon } from '../../assets/icons/mute-bel.svg';
-import { ReactComponent as PhoneIcon } from '../../assets/icons/phone.svg';
-import { ReactComponent as TrashIcon } from '../../assets/icons/trash.svg';
-import { ReactComponent as SearchIcon } from '../../assets/icons/search-icon.svg';
+
+import {
+  MuteIcon, PhoneIconTopPanel, TrashIcon, SearchIconTopPanel,
+} from '../../assets/icons/icons';
 
 export default function TopPanelSettings(props: { isOpen: boolean, onClose: () => void }) {
   const { isOpen, onClose } = props;
@@ -13,10 +13,10 @@ export default function TopPanelSettings(props: { isOpen: boolean, onClose: () =
   return (
     <nav className={isOpen ? 'top-panel-popup active' : 'top-panel-popup'} id="top-panel-popup" onMouseLeave={() => onClose()}>
       <div className="search-wrapper">
-        <PopupMenuItem label="Search" icon={<SearchIcon />} handleClick={click} />
+        <PopupMenuItem label="Search" icon={<SearchIconTopPanel />} handleClick={click} />
       </div>
       <PopupMenuItem label="Mute" icon={<MuteIcon />} handleClick={click} />
-      <PopupMenuItem label="Call" icon={<PhoneIcon />} handleClick={click} />
+      <PopupMenuItem label="Call" icon={<PhoneIconTopPanel />} handleClick={click} />
       <PopupMenuItem label="Delete chat" icon={<TrashIcon />} handleClick={click} />
     </nav>
   );
