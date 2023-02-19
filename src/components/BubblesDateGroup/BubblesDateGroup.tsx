0d989af5da6cfa7ea0dba0210459581725a53a9b
ilya <shakurov.ia@gmail.com>
2023-02-19 20:07:30 +0300

    feat: add auto-scrolling, which does not work

--- src/components/BubblesDateGroup/BubblesDateGroup.tsx
+++ src/components/BubblesDateGroup/BubblesDateGroup.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable array-callback-return */
 import { useEffect, useContext, useState } from 'react';
-import { doc, onSnapshot } from '@firebase/firestore';
-import { db } from '../../firebaseConfig';
+import { DocumentData } from '@firebase/firestore';
 import BubblesDate from '../BubblesDate/BubblesDate';
 import BubblesMessage from '../BubblesMessage/BubblesMessage';
 import './BubblesDateGroup.scss';
@@ -9,38 +8,30 @@ import { ActiveChatContext } from '../../context/ActiveChatContext';
 import { Message, User } from '../../types';
 import { AuthContext } from '../../context/AuthContext';
 
-export function BubblesDateGroup(props: {date: string}) {
+interface BubblesDateGroupProps {
+  date: string,
+  messages: DocumentData,
+}
+
+export function BubblesDateGroup({ date, messages }: BubblesDateGroupProps) {
   const { activeChatID } = useContext(ActiveChatContext);
   const currentUser: User = useContext(AuthContext) as User;
   const [chatsArr, setMessageArr] = useState([]);
-  const { date } = props;
-  const getData = async () => {
-    if (activeChatID) {
-      onSnapshot(doc(db, 'chats', activeChatID), (d) => {
-        const data = d.data();
-        if (!data) {
-          console.log('Hey! There are no messages here.');
-          return;
-        }
-
-        setMessageArr(data.messages
-          .map((message: Message) => (
-            <BubblesMessage
-              message={message.text}
-              time={`${(new Date(message.date.seconds * 1000)).getHours().toString().padStart(2, '0')}:${new Date(message.date.seconds * 1000).getMinutes().toString().padStart(2, '0')}`}
-              isRead
-              isCurrenUser={message.senderID === currentUser?.uid}
-              key={message.id}
-              imageUrl={message.imageUrl}
-            />
-          )));
-      });
-    }
-  };
 
   useEffect(() => {
-    getData();
-  }, [activeChatID]);
+    setMessageArr(messages
+      .map((message: Message) => (
+        <BubblesMessage
+          message={message.text}
+          time={`${(new Date(message.date.seconds * 1000)).getHours().toString().padStart(2, '0')}
+                :${new Date(message.date.seconds * 1000).getMinutes().toString().padStart(2, '0')}`}
+          isRead
+          isCurrenUser={message.senderID === currentUser?.uid}
+          key={message.id}
+          imageUrl={message.imageUrl}
+        />
+      )));
+  }, [messages, activeChatID]);
 
   return (
     <section className="bubbles-date-group">
--- src/components/ChatWindow/ChatWindow.tsx
+++ src/components/ChatWindow/ChatWindow.tsx
@@ -1,4 +1,8 @@
-import { useContext } from 'react';
+import {
+  useContext, useRef, useEffect, useState,
+} from 'react';
+import { doc, onSnapshot, DocumentData } from '@firebase/firestore';
+import { db } from '../../firebaseConfig';
 import { ActiveChatContext } from '../../context/ActiveChatContext';
 import { SendImageContext } from '../../context/SendImageContext';
 import MessageInput from '../MessageInput/MessageInput';
@@ -11,11 +15,55 @@ function ChatWindow() {
   const { popap } = useContext(SendImageContext);
   const { activeChatID } = useContext(ActiveChatContext);
 
+  const [messages, setMessages] = useState<DocumentData>([]);
+
+  useEffect(() => {
+    if (activeChatID) {
+      onSnapshot(doc(db, 'chats', activeChatID), (d) => {
+        const data = d.data();
+        if (!data) {
+          console.log('Hey! There are no messages here.');
+          return;
+        }
+        setMessages(data.messages);
+      });
+    }
+  }, [activeChatID]);
+
+  const messageContainerRef = useRef<HTMLDivElement>(null);
+  const messageContainer = messageContainerRef.current;
+  const [scrolledToBottom, setScrolledToBottom] = useState(true);
+
+  const handleScroll = () => {
+    console.log('scroll');
+    const { scrollTop, scrollHeight, clientHeight } = messageContainerRef.current as HTMLDivElement;
+    if (scrollTop + clientHeight === scrollHeight) {
+      setScrolledToBottom(true);
+    } else {
+      setScrolledToBottom(false);
+    }
+  };
+
+  useEffect(() => {
+    if (messageContainer) {
+      messageContainer.scrollTop = messageContainer.scrollHeight;
+    }
+    messageContainer?.addEventListener('scroll', handleScroll);
+    return () => messageContainer?.removeEventListener('scroll', handleScroll);
+  }, [activeChatID]);
+
+  useEffect(() => {
+    console.log(scrolledToBottom);
+    if (messageContainer && scrolledToBottom) {
+      messageContainer.scrollTop = messageContainer.scrollHeight;
+    }
+  }, [messages]);
+
   return (
     <div className="chat-window">
       {activeChatID && (
-      <div className="chat-window__wrapper">
-        <BubblesDateGroup date="Today" />
+      <div className="chat-window__wrapper" ref={messageContainerRef}>
+        <BubblesDateGroup date="Today" messages={messages} />
       </div>
       )}
       {activeChatID && <MessageInput />}
