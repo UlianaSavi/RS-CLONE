00ffdbf96cf049f9049fa1424a0969c86a63b3a8
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-02-20 17:45:21 +0300

    feat: add pattern for password in sign up form

--- src/pages/Form/Form.tsx
+++ src/pages/Form/Form.tsx
@@ -57,7 +57,7 @@ function Form({ mode }: FormProps) {
   <>
     <FormInput type="text" id="name" label="Username" value="" setValue={setName} required />
     <FormInput type="email" id="email" label="Email" value="" setValue={setEmail} pattern="^[A-Za-z0-9]*[@][A-Za-z0-9]*[.][A-Za-z]*" title="Please enter the correct email address. For example: duck@gmail.com" required />
-    <FormInput type="password" id="password" label="Password" value="" setValue={setPassword} required />
+    <FormInput type="password" id="password" label="Password" value="" setValue={setPassword} pattern=".{6,}" title="At least 6 characters are required" required />
   </>,
     },
   };
