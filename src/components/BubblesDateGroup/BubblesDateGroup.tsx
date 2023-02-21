c896b848bf0f5bd7d6d155b5bc1465bd3b461c8b
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-02-21 16:44:39 +0300

    feat: add today and yesterday

--- src/components/BubblesDateGroup/BubblesDateGroup.tsx
+++ src/components/BubblesDateGroup/BubblesDateGroup.tsx
@@ -7,9 +7,23 @@ interface BubblesDateGroupProps {
 }
 
 export function BubblesDateGroup({ date, messagesArr }: BubblesDateGroupProps) {
+  const today = new Date().toString().split(' ').splice(1, 2)
+    .join(' ');
+  const yesterdayDate = new Date();
+  yesterdayDate.setDate(yesterdayDate.getDate() - 1);
+  const yesterday = yesterdayDate.toString().split(' ').splice(1, 2)
+    .join(' ');
+
+  let dateForView = date;
+  if (date === today) {
+    dateForView = 'Today';
+  } else if (date === yesterday) {
+    dateForView = 'Yesterday';
+  }
+
   return (
     <section className="bubbles-date-group">
-      <BubblesDate date={date} />
+      <BubblesDate date={dateForView} />
       <div className="bubbles-date-group__messages-wrapper">
         <div className="bubbles-date-group__user-messages-wrapper">
           {messagesArr}
