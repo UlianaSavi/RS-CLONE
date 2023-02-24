69aff98520aca60ce5510a25aa86385523ef45ab
UlianaSavi <uljasha.sav@gmail.com>
2023-02-24 16:13:16 +0600

    feat: add adaptive and info for user sidebar

--- src/components/UserSidebarInfo/UserSidebarInfo.tsx
+++ src/components/UserSidebarInfo/UserSidebarInfo.tsx
@@ -1,31 +1,62 @@
 import { useContext, useEffect, useState } from 'react';
-import { AuthContext } from '../../context/AuthContext';
+import {
+  doc,
+  DocumentData,
+  getDoc,
+  onSnapshot,
+} from '@firebase/firestore';
 import type { User } from '../../types';
 import avatarPlaceholder from '../../assets/icons/avatar-placeholder.png';
 import { InfoIcon, NameUserIcon } from '../../assets/icons/icons';
 import PopupMenuItem from '../PopupMenuItem/PopupMenuItem';
 import './UserSidebarInfo.scss';
+import { ActiveChatContext } from '../../context/ActiveChatContext';
+import { UserContext } from '../../context/UserContext';
+import { convertTimestamp } from '../../hooks/timestampConverter';
+import { db } from '../../firebaseConfig';
 
 function UserSidebarInfo() {
-  const currentUser: User = useContext(AuthContext) as User;
-  const [user, setUser] = useState(currentUser);
+  const { activeChatID } = useContext(ActiveChatContext);
+  const { userID } = useContext(UserContext);
+  const [userInfo, setUserInfo] = useState<User | null>(null);
+  const [isOnline, setIsOnline] = useState(userInfo?.isOnline || false);
+  const [lastSeen, setLastSeen] = useState('');
+  const setData = async (data: DocumentData) => {
+    setIsOnline(data.isOnline);
+    if (!data.isOnline && data.lastVisitAt) {
+      setLastSeen(convertTimestamp(data.lastVisitAt));
+    }
+  };
 
+  const getData = async () => {
+    if (activeChatID) {
+      const user = await getDoc(doc(db, 'users', userID));
+      const data = user.data() as User;
+      setUserInfo(data);
+      setData(data);
+    }
+  };
   useEffect(() => {
-    setUser(user);
-  }, [currentUser.photoURL]);
+    getData();
+    onSnapshot(doc(db, 'users', userID), (d) => {
+      const data = d.data();
+      if (!data || !userInfo) return;
+      setData(data);
+    });
+  }, [activeChatID]);
 
   return (
     <div className="user-sidebar-info">
       <div className="user-sidebar-info__wrapper">
-        <img className="user-sidebar-info__wrapper__ava" src={user.photoURL || avatarPlaceholder} alt="User" />
-        <div className="user-sidebar-info__wrapper__name">{currentUser.displayName}</div>
-        <div className="user-sidebar-info__wrapper__status">{currentUser.isOnline || 'online'}</div>
+        <img className="user-sidebar-info__wrapper__ava" src={userInfo?.photoURL || avatarPlaceholder} alt="User" />
+        <div className="user-sidebar-info__wrapper__name">{userInfo?.displayName}</div>
+        <div className="user-sidebar-info__wrapper__status">{isOnline ? 'Online' : `Last seen ${lastSeen}`}</div>
       </div>
       <div className="user-sidebar-info__item">
-        <PopupMenuItem label={currentUser.email} icon={<NameUserIcon />} title="Email" />
+        <PopupMenuItem label={userInfo?.email || 'Label'} icon={<NameUserIcon />} title="Email" />
       </div>
       <div className="user-sidebar-info__item">
-        <PopupMenuItem label={currentUser.displayName} icon={<NameUserIcon />} title="Username" />
+        <PopupMenuItem label={userInfo?.displayName || 'User'} icon={<NameUserIcon />} title="Username" />
       </div>
       <div className="user-sidebar-info__item">
         <PopupMenuItem label="About me info" icon={<InfoIcon />} title="About" />
