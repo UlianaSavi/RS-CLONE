154d0697f550bb0aad1226c1516e5c3f2a35a5c5
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-02-24 21:31:11 +0300

    feat: add changing theme on click from settings popup

--- src/components/SettingsPopap/SettingsPopap.tsx
+++ src/components/SettingsPopap/SettingsPopap.tsx
@@ -1,18 +1,27 @@
+import { useContext } from 'react';
 import PopupMenuItem from '../PopupMenuItem/PopupMenuItem';
 import { ReactComponent as SavedMessIcon } from '../../assets/icons/bookmark.svg';
 import { ReactComponent as SettingIcon } from '../../assets/icons/setting-icon.svg';
+import { DarkModeIcon } from '../../assets/icons/icons';
 import './SettingsPopap.scss';
+import { ThemeContext } from '../../context/ThemeContext';
 
 function SettingsMenu(props: { isOpen: boolean, onClose: () => void,
   onSidebarChange: () => void }) {
   const { isOpen, onClose, onSidebarChange } = props;
+  const { isDark, setIsDark } = useContext(ThemeContext);
 
   const click = () => null;
+
+  const changeTheme = () => {
+    setIsDark(!isDark);
+  };
 
   return (
     <nav className={isOpen ? 'popup active' : 'popup'} id="popup" onMouseLeave={() => onClose()}>
       <PopupMenuItem label="Saved Messages" icon={<SavedMessIcon />} handleClick={click} />
       <PopupMenuItem label="Settings" icon={<SettingIcon />} handleClick={onSidebarChange} />
+      <PopupMenuItem label="Dark mode" icon={<DarkModeIcon />} handleClick={changeTheme} />
     </nav>
   );
 }
