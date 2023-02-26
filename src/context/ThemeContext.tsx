0a88a3c1ba79a1cd884df736caa918712661c884
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-02-26 13:02:38 +0300

    fix: default dark theme

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -19,7 +19,7 @@ export const ThemeContext = createContext<IContext>({
 });
 
 export function ThemeProvider({ children }: ProviderProps) {
-  const [isDark, setIsDark] = useState(false);
+  const [isDark, setIsDark] = useState(true);
 
   const value = useMemo(() => ({ isDark, setIsDark }), [isDark, setIsDark]);
 
